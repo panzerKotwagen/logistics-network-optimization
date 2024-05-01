de386e441ceb9844a1b855fe2400e85d7a2722d7
kotb <kotyachy.andegraund@gmail.com>
2024-05-01 16:53:26 +0300

    Add entities

---
+++ src/main/java/ru/kotb/lno/db/entity/MotorRoad.java
@@ -0,0 +1,62 @@
+package ru.kotb.lno.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/**
+ * The class describes a highway on the logistics network
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "roads")
+public class MotorRoad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    /**
+     * The notation
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * The notional amount of all expenses incurred during the trip
+     */
+    @Column(name = "cost")
+    private int cost;
+
+    /**
+     * Expected travel time
+     */
+    @Column(name = "time")
+    private int time;
+
+    /**
+     * One of the cities connected to the road
+     */
+    @OneToOne
+    @JoinColumn(name = "first_city_id")
+    private City firstCity;
+
+    /**
+     * The second of the cities connected to the road
+     */
+    @OneToOne
+    @JoinColumn(name = "second_city_id")
+    private City secondCity;
+}
