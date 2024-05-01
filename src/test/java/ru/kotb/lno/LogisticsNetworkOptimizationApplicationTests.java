f5d38bbcd57b99ec2672c030827bdceb1a03afbd
kotb <kotyachy.andegraund@gmail.com>
2024-05-01 11:59:54 +0300

    Create project skeleton

---
+++ src/test/java/ru/kotb/lno/LogisticsNetworkOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kotb.lno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsNetworkOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
