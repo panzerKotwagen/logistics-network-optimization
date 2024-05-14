f401a5bf7668f2614ef42b4e3bcf794e15f336ff
Яблоков Виктор Андреевич <v.yablokov@nicetu.spb.ru>
2024-05-14 22:40:16 +0300

    Replace individual weight variables with an array

--- src/main/java/ru/kotb/lno/graph/components/Edge.java
+++ src/main/java/ru/kotb/lno/graph/components/Edge.java
@@ -1,43 +1,47 @@
 package ru.kotb.lno.graph.components;
 
-import lombok.AllArgsConstructor;
+import java.util.Objects;
+
+import org.jgrapht.graph.DefaultEdge;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.jgrapht.graph.DefaultEdge;
-
-import java.util.Objects;
-
 
 /**
  * The class describes a graph edge
  */
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Edge extends DefaultEdge {
+
+    private final int WEIGHT_COUNT = 2;
 
     /**
      * Edge name
      */
     private String name;
 
-
     /**
-     * The edge weight №1
+     * Weight array
      */
-    private int w1;
+    private final int[] weights = new int[WEIGHT_COUNT];
 
-    /**
-     * The edge weight №2
-     */
-    private int w2;
+    public Edge(String name, int w1, int w2) {
+        this.name = name;
+        weights[0] = w1;
+        weights[1] = w2;
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Edge edge)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Edge edge)) {
+            return false;
+        }
         return Objects.equals(getSource(), edge.getSource()) && Objects.equals(getTarget(), edge.getTarget());
     }
 
