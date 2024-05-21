6a6323ce686e20628b021cbf60911a89074a7239
Яблоков Виктор Андреевич <v.yablokov@nicetu.spb.ru>
2024-05-21 10:03:55 +0300

    Add tests

--- src/main/java/ru/kotb/lno/graph/algorithms/DijkstraShortestPath.java
+++ src/main/java/ru/kotb/lno/graph/algorithms/DijkstraShortestPath.java
@@ -5,9 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.kotb.lno.graph.Graph;
 import ru.kotb.lno.graph.components.Edge;
-import ru.kotb.lno.optimization.Solver;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -18,13 +16,21 @@ import java.util.Queue;
  * The class describing Dijkstra's algorithm for finding the shortest path
  */
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class DijkstraShortestPath {
 
-    public Map<String, Double> findShortestPath(Graph graph, String firstNodeName) {
+    private static String getOppositeNode(Edge adjacentEdge, NodeAndDistance current) {
+        String source = adjacentEdge.getSourceNode();
+        String target = adjacentEdge.getTargetNode();
+        if (source.equals(current.node)) {
+            return target;
+        } else {
+            return source;
+        }
+    }
+
+    public Map<String, Double> findShortestPath(Graph graph, String firstNodeName, int weightNumber) {
         Map<String, Double> distances = new LinkedHashMap<>();
-        Map<String, Boolean> marked = new HashMap<>();
 
         for (String vertex : graph.nodeNamesSet()) {
             distances.put(vertex, Double.MAX_VALUE);
@@ -36,10 +42,6 @@ public class DijkstraShortestPath {
 
         while (!queue.isEmpty()) {
             NodeAndDistance current = queue.poll();
-            if (marked.containsKey(current.node)) {
-                continue;
-            }
-            marked.put(current.node, true);
 
             //Processing only the vertex with the smallest distance
             if (current.distance > distances.get(current.node)) {
@@ -48,7 +50,8 @@ public class DijkstraShortestPath {
 
             for (Edge adjacentEdge : graph.getAdjacentEdges(current.node)) {
                 String neighbourNode = getOppositeNode(adjacentEdge, current);
-                double distance = current.distance + adjacentEdge.getWeights()[0];
+
+                double distance = current.distance + adjacentEdge.getWeights()[weightNumber];
 
                 //We consider this new path only if it is better than
                 // any path we have found so far
@@ -62,15 +65,6 @@ public class DijkstraShortestPath {
         return distances;
     }
 
-    private static String getOppositeNode(Edge adjacentEdge, NodeAndDistance current) {
-        String source = adjacentEdge.getSourceNode();
-        String target = adjacentEdge.getTargetNode();
-        if (source.equals(current.node)) {
-            return target;
-        } else {
-            return source;
-        }
-    }
 
     @AllArgsConstructor
     private static class NodeAndDistance {
--- src/test/java/ru/kotb/lno/graph/OptimalPathTest.java
+++ src/test/java/ru/kotb/lno/graph/OptimalPathTest.java
@@ -4,10 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.kotb.lno.graph.algorithms.DijkstraShortestPath;
 import ru.kotb.lno.graph.impl.JGraphT;
-import ru.kotb.lno.optimization.GlobalCriteria;
-import ru.kotb.lno.optimization.Solver;
-import ru.kotb.lno.optimization.schemes.CompromiseScheme;
-import ru.kotb.lno.optimization.schemes.impl.Convolution;
 
 import java.util.Map;
 
@@ -20,45 +16,73 @@ public class OptimalPathTest {
 
     @Test
     void findOptimalPath() {
-        String[] nodes = {"S", "1", "2", "3", "T"};
+        String[] nodes = {"S", "2", "3", "4", "5", "T"};
         for (String node : nodes) {
             graph.addNode(node);
         }
 
-        graph.addEdge("S", "1", "", 1, 1);
-        graph.addEdge("S", "2", "", 1, 1);
-        graph.addEdge("S", "3", "", 1, 1);
+        graph.addEdge("S", "T", "", 14, 1);
+        graph.addEdge("S", "2", "", 7, 1);
+        graph.addEdge("S", "3", "", 9, 1);
 
-        graph.addEdge("1", "T", "", 10, 1);
+        graph.addEdge("2", "3", "", 10, 1);
+        graph.addEdge("2", "4", "", 15, 1);
 
-        graph.addEdge("2", "T", "", 5, 5);
+        graph.addEdge("3", "T", "", 2, 1);
+        graph.addEdge("3", "4", "", 11, 1);
 
-        graph.addEdge("3", "T", "", 1, 10);
+        graph.addEdge("5", "T", "", 9, 1);
+        graph.addEdge("5", "4", "", 6, 1);
 
-        double[] weights1 = new double[]{1, 0.1};
-        double[] weights2 = new double[]{0.1, 1};
-        double[] weights3 = new double[]{0.5, 0.5};
+        Map<String, Double> distances = optimalPath.findShortestPath(graph, "S", 0);
+        double distanceTo2 = distances.get("2");
+        double distanceTo3 = distances.get("3");
+        double distanceTo4 = distances.get("4");
+        double distanceTo5 = distances.get("5");
+        double distanceToT = distances.get("T");
 
-//        CompromiseScheme scheme1 = new Convolution(weights1);
-//        CompromiseScheme scheme2 = new Convolution(weights2);
-//        CompromiseScheme scheme3 = new Convolution(weights3);
-//
-//        optimalPath.setSolver(new Solver(scheme1));
-//        Map<String, GlobalCriteria> distances = optimalPath.findOptimalPath(graph, "S");
-//        GlobalCriteria distanceToT = distances.get("T");
-//        GlobalCriteria expected = new GlobalCriteria(2, 11);
-//        Assertions.assertThat(distanceToT).isEqualTo(expected);
-//
-//        optimalPath.setSolver(new Solver(scheme2));
-//        distances = optimalPath.findOptimalPath(graph, "S");
-//        distanceToT = distances.get("T");
-//        expected = new GlobalCriteria(11, 2);
-//        Assertions.assertThat(distanceToT).isEqualTo(expected);
-//
-//        optimalPath.setSolver(new Solver(scheme3));
-//        distances = optimalPath.findOptimalPath(graph, "S");
-//        distanceToT = distances.get("T");
-//        expected = new GlobalCriteria(6, 6);
-//        Assertions.assertThat(distanceToT).isEqualTo(expected);
+        Assertions.assertThat(distanceTo2).isEqualTo(7);
+        Assertions.assertThat(distanceTo3).isEqualTo(9);
+        Assertions.assertThat(distanceTo4).isEqualTo(20);
+        Assertions.assertThat(distanceTo5).isEqualTo(20);
+        Assertions.assertThat(distanceToT).isEqualTo(11);
+    }
+
+    @Test
+    void findOptimalPath1() {
+        String[] nodes = {"S", "11", "12", "21", "22", "23", "31", "32", "T"};
+        for (String node : nodes) {
+            graph.addNode(node);
+        }
+
+        graph.addEdge("S", "11", "", 1, 1);
+        graph.addEdge("S", "12", "", 1, 1);
+
+        graph.addEdge("11", "21", "", 5, 4);
+        graph.addEdge("11", "22", "", 3, 8);
+        graph.addEdge("11", "23", "", 6, 3);
+
+        graph.addEdge("12", "21", "", 8, 2);
+        graph.addEdge("12", "22", "", 6, 4);
+        graph.addEdge("12", "23", "", 5, 5);
+
+        graph.addEdge("21", "31", "", 7, 3);
+
+        graph.addEdge("22", "31", "", 5, 5);
+        graph.addEdge("22", "32", "", 2, 7);
+
+        graph.addEdge("23", "32", "", 9, 6);
+
+        graph.addEdge("31", "T", "", 10, 5);
+        graph.addEdge("32", "T", "", 5, 10);
+
+        Map<String, Double> w1Distances = optimalPath.findShortestPath(graph, "S", 0);
+        Map<String, Double> w2Distances = optimalPath.findShortestPath(graph, "S", 1);
+
+        double w1DistanceToT = w1Distances.get("T");
+        double w2DistanceToT = w2Distances.get("T");
+
+        Assertions.assertThat(w1DistanceToT).isEqualTo(11);
+        Assertions.assertThat(w2DistanceToT).isEqualTo(11);
     }
 }
